9fa0c69da326d17394eabb42ddd317da108963b4
Brandon Kaminski <brankam126@gmail.com>
2026-01-18 14:26:44 -0500

    feat: first commit with working UNO + fonts!

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,9 @@
+export default function Homepage() {
+  return (
+    <div>
+      <h1 className="text-4xl font-averia text-cerulean">
+        Welcome to the Event Site
+      </h1>
+    </div>
+  );
+}
---
+++ uno.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'unocss';
+import presetWind4 from '@unocss/preset-wind4';
+
+export default defineConfig({
+  presets: [presetWind4()],
+  theme: {
+    colors: {
+      cerulean: '#007ba7',
+      brankamyellow: '#fec201',
+      cherryred: '#ff2b2b',
+      maingray: '#999999',
+    },
+    font: {
+      averia: '"Averia Libre", cursive',
+      homenaje: '"Homenaje", serif',
+    },
+  },
+});
